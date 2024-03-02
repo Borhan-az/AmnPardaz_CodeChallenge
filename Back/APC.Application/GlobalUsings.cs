eedd48029cf4a16f9cd0080515ac96a38473b725
Borhan Azhdari <b.ajdari@pardis.ir>
2024-03-02 17:35:19 +0330

    global usings updated

---
+++ Back/APC.Application/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using APC.Application.Common.Interfaces;
+global using Microsoft.EntityFrameworkCore;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using MediatR;
