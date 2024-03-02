c26a29c5acdbd8a6922b1a49387ac21bc0ef855d
Borhan Azhdari <b.ajdari@pardis.ir>
2024-03-02 17:34:51 +0330

    dbcontext interface added

---
+++ Back/APC.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using APC.Domain.Entities.Users;
+
+namespace APC.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<Domain.Entities.TodoLists.Task> Tasks { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
