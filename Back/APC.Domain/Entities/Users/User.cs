73376943c80f34685599202a50b87bc7ca9231c7
Borhan Azhdari <b.ajdari@pardis.ir>
2024-03-02 17:36:01 +0330

    user entity updated

--- Back/APC.Domain/Entities/Users/User.cs
+++ Back/APC.Domain/Entities/Users/User.cs
@@ -2,9 +2,20 @@
 {
     public class User : BaseAuditableEntity
     {
-        public required string UserName { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string PasswordSalt { get; set; }
+        public User() : base()
+        {
+        }
+        public User(string userName, string email, string password, string salt)
+        {
+            UserName = userName;
+            Email = email;
+            PasswordHash = password;
+            PasswordSalt = salt;
+
+        }
     }
 }
