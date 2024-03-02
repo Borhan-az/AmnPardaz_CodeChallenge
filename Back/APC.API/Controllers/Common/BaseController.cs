b0855a0da4e9d9172d669866d741d802992c42d1
Btxhash <hashbtx@gmail.com>
2024-03-03 02:40:07 +0330

    mediator added

--- Back/APC.API/Controllers/Common/BaseController.cs
+++ Back/APC.API/Controllers/Common/BaseController.cs
@@ -1,10 +1,13 @@
-﻿namespace APC.API.Controllers.Common
+﻿using MediatR;
+
+namespace APC.API.Controllers.Common
 {
     [Route("api/[controller]")]
     [ApiController]
     public class BaseController : ControllerBase
     {
-
+        private IMediator mediator;
+        protected IMediator _mediator => mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 
     }
 }
