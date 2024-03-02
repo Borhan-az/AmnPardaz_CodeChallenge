e8742e5b87e9450ed39280eb4cfe6ddd0032349e
Btxhash <hashbtx@gmail.com>
2024-03-03 02:42:15 +0330

    login and register endpoints updated

--- Back/APC.API/Controllers/AuthController.cs
+++ Back/APC.API/Controllers/AuthController.cs
@@ -1,22 +1,35 @@
-﻿namespace APC.API.Controllers
+﻿using APC.Application.Auth.Commands.RegisterUser;
+using APC.Application.Common.JWT;
+
+namespace APC.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : BaseController
     {
+        private readonly ITokenService _token;
+        public AuthController(ITokenService token)
+        {
+            _token = token;
+        }
 
         [HttpPost("Login")]
         public async Task<IActionResult> Login()
         {
-            return Ok();
+            Guid guid = Guid.NewGuid();
+            var token = await _token.GenerateToken(guid, "borhan");
+
+
+            return Ok(token);
         }
 
         [HttpPost("Register")]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register(RegisterUserCommand register)
         {
+            await _mediator.Send(register);
+
             return Ok();
         }
-
 
     }
 }
