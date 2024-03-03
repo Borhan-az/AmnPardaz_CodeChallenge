808a844255ce36da2d8c971ec55526791f55a26d
Btxhash <hashbtx@gmail.com>
2024-03-04 02:22:38 +0330

    updated

--- Front/apc-app/src/App.js
+++ Front/apc-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-// import TodoList from "./pages/TodoList"
-// import TodoCreate from "./pages/TodoCreate"
+import TodoList from "./pages/TodoList"
+import UserList from "./pages/UserList"
+import TodoCreate from "./pages/TodoCreate"
 // import TodoUpdate from "./pages/TodoUpdate"
 import Login from "./pages/Login"
 import Registration from "./pages/Registration"
@@ -12,8 +13,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/signup" element={<Registration />} />
-        {/* <Route exact path="/dashboard" element={<TodoList />} /> */}
-        {/* <Route path="/create" element={<TodoCreate />} /> */}
+        <Route exact path="/dashboard" element={<TodoList />} />
+        <Route path="/create" element={<TodoCreate />} />
+        <Route path="/users" element={<UserList />} />
         {/* <Route path="/update/:id" element={<TodoUpdate />} /> */}
       </Routes>
     </Router>
