e26fe9d4c7eb47cff2485d758e0f908a538eefc3
Borhan Azhdari <b.ajdari@pardis.ir>
2024-03-03 16:28:10 +0330

    app.js imporved and updated

---
+++ Front/apc-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import TodoList from "./pages/TodoList"
+// import TodoCreate from "./pages/TodoCreate"
+// import TodoUpdate from "./pages/TodoUpdate"
+import Login from "./pages/Login"
+import Registration from "./pages/Registration"
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/signup" element={<Registration />} />
+        {/* <Route exact path="/dashboard" element={<TodoList />} /> */}
+        {/* <Route path="/create" element={<TodoCreate />} /> */}
+        {/* <Route path="/update/:id" element={<TodoUpdate />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
