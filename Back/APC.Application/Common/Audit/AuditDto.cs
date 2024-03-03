973d26230f86c1edcf56449b95c561537f6f1f8b
Borhan Azhdari <b.ajdari@pardis.ir>
2024-03-03 16:30:22 +0330

    auditDto added

---
+++ Back/APC.Application/Common/Audit/AuditDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APC.Application.Common.Audit
+{
+    public class AuditDto
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public AuditDto(Guid userId, string userName)
+        {
+            UserId = userId;
+            UserName = userName;
+        }
+    }
+}
